96c7591d87f41104bffda5a70ce103d31271e6fb
kapable <skjoldenstore@gmail.com>
2021-01-15 14:51:57 +0900

    edit polifill

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
-import 'core-js/stable';
+import 'core-js';
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom';
