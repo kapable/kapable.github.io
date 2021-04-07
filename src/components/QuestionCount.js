ccf1d4888505620e7c1178a60c740a17ff2aaa81
kapable <skjoldenstore@gmail.com>
2021-04-07 12:58:40 +0900

    fix progress bar to new one

--- src/components/QuestionCount.js
+++ src/components/QuestionCount.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ProgressBar } from 'react-bootstrap';
+import ProgressBar from "@ramonak/react-progress-bar";
 
 class QuestionCount extends Component {
     
@@ -7,7 +7,10 @@ class QuestionCount extends Component {
         return(
             <div className="question-progress">
                 <div className='progress-bar' >
-                    <ProgressBar now={this.props.quizCount / this.props.totalCount * 100} variant="danger" />
+                    <ProgressBar
+                        completed={this.props.quizCount / this.props.totalCount * 100 + 10}
+                        isLabelVisible={false} baseBgColor='#fbf7f2' bgColor='#b70048'
+                        height='10px' width='100%' transitionDuration='.3s'/>
                 </div>
                 <div className="question-count">
                     {this.props.quizCount+1} / {this.props.totalCount}
