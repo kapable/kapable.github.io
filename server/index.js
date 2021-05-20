72ca9fc660c1de96f240c42b6d64b936156bdcdc
kapable <skjoldenstore@gmail.com>
2021-05-20 20:44:37 +0900

    add auth middleware and logout method

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/dev');
+const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
 const app = express();
 const port = 5000;
@@ -59,5 +60,26 @@ app.post('/api/users/login', (req, res) => {
         })
     })    
 })
+
+app.get('/api/users/auth', auth, (req, res) => {
+    // 여기까지 미들웨어를 통과해 왔다는 의미는, Authentication === True
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token:"" }, (err, user) => {
+        if(err) return res.json({ success: false, err });
+        return res.status(200).send({ success: true });
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- server/models/User.js
+++ server/models/User.js
@@ -75,6 +75,19 @@ userSchema.methods.generateToken = function(cb) {
     })
 };
 
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    // decoding Token
+    jwt.verify(token, 'secretToekn', function(err, decoded) {
+        // 유저 아이디를 이용해서 유저를 찾은 다음에 클라이언트에서 가져온 token과 DB에 보관된 token이 일치하는지 확인
+        user.findOne({ "_id":decoded, "token":token }, function(err, user) {
+            if(err) return cb(err);
+            cb(null, user);
+        });
+    });
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = { User }
