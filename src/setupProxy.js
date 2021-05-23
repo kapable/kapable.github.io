f2b4e13d49fcb70da38eecb1ed67bbe32f4b0b44
kapable <skjoldenstore@gmail.com>
2021-05-23 21:09:25 +0900

    update CORS

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
         '/api',
         createProxyMiddleware({
             target: 'http://localhost:5000',
-            changeOrigin: true
+            changeOrigin: true,
         })
     )
 };
