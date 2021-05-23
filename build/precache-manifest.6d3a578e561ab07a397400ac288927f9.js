fdbe3f0ce2b5ee82344c52c3ba298ec344594da2
kapable <skjoldenstore@gmail.com>
2021-05-23 22:03:23 +0900

    add build files

---
+++ build/precache-manifest.6d3a578e561ab07a397400ac288927f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42240d8ee2341b227972c727167d9581",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00416c59e9fe1e592e55",
+    "url": "/static/css/main.c4b4f562.chunk.css"
+  },
+  {
+    "revision": "737674cd955beb7ef7ef",
+    "url": "/static/js/2.5e8df7b4.chunk.js"
+  },
+  {
+    "revision": "6655a25e593bcb6c1d6d",
+    "url": "/static/js/3.6adfe980.chunk.js"
+  },
+  {
+    "revision": "00416c59e9fe1e592e55",
+    "url": "/static/js/main.c3cef84d.chunk.js"
+  },
+  {
+    "revision": "63fe9c590fe9ad5987e7",
+    "url": "/static/js/runtime-main.e90ef657.js"
+  },
+  {
+    "revision": "21ffb6bbef31e30abb8be384eb553611",
+    "url": "/static/media/main-header.21ffb6bb.png"
+  }
+]);
