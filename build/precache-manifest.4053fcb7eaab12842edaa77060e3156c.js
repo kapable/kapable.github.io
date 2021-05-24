58eb9659fb028d205ca703bc8f8623738818fd65
kapable <skjoldenstore@gmail.com>
2021-05-24 09:42:03 +0900

    fix untracked files

---
+++ build/precache-manifest.4053fcb7eaab12842edaa77060e3156c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a27879a6ef0a002da8f45efc91e1f2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d56a21f666d2ca0a45",
+    "url": "/static/css/main.c4b4f562.chunk.css"
+  },
+  {
+    "revision": "593ae6d314ce3a43c9ec",
+    "url": "/static/js/2.3a3dd800.chunk.js"
+  },
+  {
+    "revision": "a4d64d8080146665eb1e",
+    "url": "/static/js/3.05dd458b.chunk.js"
+  },
+  {
+    "revision": "19d56a21f666d2ca0a45",
+    "url": "/static/js/main.02c94619.chunk.js"
+  },
+  {
+    "revision": "40ddd44161cbb3faa58a",
+    "url": "/static/js/runtime-main.1ded8bc9.js"
+  },
+  {
+    "revision": "21ffb6bbef31e30abb8be384eb553611",
+    "url": "/static/media/main-header.21ffb6bb.png"
+  }
+]);
