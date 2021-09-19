3170eeba9217f1b518254f218064e623b753c04f
kapable <skjoldenstore@gmail.com>
2021-09-19 21:56:04 +0900

    Updates

---
+++ precache-manifest.0159fe30740cf655472f51c5c9d41d56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44358bce673df6f6b18fb2e111e7170",
+    "url": "/index.html"
+  },
+  {
+    "revision": "560003f39ed5e581d35c",
+    "url": "/static/css/2.a312ce8b.chunk.css"
+  },
+  {
+    "revision": "458279c3a51a4a54b158",
+    "url": "/static/css/main.3fe93690.chunk.css"
+  },
+  {
+    "revision": "560003f39ed5e581d35c",
+    "url": "/static/js/2.4a635805.chunk.js"
+  },
+  {
+    "revision": "a45813ffeb99e14826ac",
+    "url": "/static/js/3.ba5f4406.chunk.js"
+  },
+  {
+    "revision": "458279c3a51a4a54b158",
+    "url": "/static/js/main.b1e45d5d.chunk.js"
+  },
+  {
+    "revision": "1357643ca70ec1d35d71",
+    "url": "/static/js/runtime-main.e265b66f.js"
+  },
+  {
+    "revision": "77bce2dccc428e64191cd78105a8fae4",
+    "url": "/static/media/Ktest-funny.77bce2dc.png"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "21ffb6bbef31e30abb8be384eb553611",
+    "url": "/static/media/main-header.21ffb6bb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d54c352f33f339f12d887d2cfe9f7c1.js"
+  "/precache-manifest.0159fe30740cf655472f51c5c9d41d56.js"
 );
 
 self.addEventListener('message', (event) => {
