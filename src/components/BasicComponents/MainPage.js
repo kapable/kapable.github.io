a936b4059c105ead9083319af90bd316f92c064d
kapable <skjoldenstore@gmail.com>
2021-12-13 17:02:44 +0900

    add go to jelling games banner

--- src/components/BasicComponents/MainPage.js
+++ src/components/BasicComponents/MainPage.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import MAINHEADER from '../../api/DefaultImg/main-header.png';
 import OhterLangIconsMain from '../SubComponents/OhterLangIconsMain';
 import ARTICLEBANNER from '../../api/DefaultImg/Ktest-funny.png';
+import JELLINGBANNERKOR from '../../api/DefaultImg/go-to-jelling-kor.png';
+import JELLINGBANNERENG from '../../api/DefaultImg/go-to-jelling-eng.png';
 
 class MainPage extends Component{
 
@@ -13,9 +15,26 @@ class MainPage extends Component{
                     <img className='main-header' src={MAINHEADER} alt="K-test"/>
                 </div>
                 <OhterLangIconsMain />
+                {/* Go to Jelling Games Banner */}
+                <a
+                    target='_blank'
+                    rel="noopener noreferrer"
+                    href='https://jellinggame.com/'
+                    className='to-jelling-banner'
+                > <img src={JELLINGBANNERKOR} className='test-main-img' alt="go to Jelling games" /> </a>
+                <a
+                    target='_blank'
+                    rel="noopener noreferrer"
+                    href='https://jellinggame.com/'
+                    className='to-jelling-banner'
+                > <img src={JELLINGBANNERENG} className='test-main-img' alt="go to Jelling games" /> </a>
+
+                {/* Go to K-funny articles */}
                 <Link to='/kfunny/humor/' className="main-link-block" key="article-banner">
                     <img className="test-main-img" src={ARTICLEBANNER} alt="KTEST-Funny" />
                 </Link>
+
+                {/* Main Test Banners */}
                 <div className="main-link-div">
                     {this.props.all_tests_url.map((item)=>(
                         <Link to={item[0]} className="main-link-block" key={item[0].replaceAll('/','')}>
