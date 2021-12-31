4488d59b0876a9a8d49abad7e06fcddbacb01244
kapable <skjoldenstore@gmail.com>
2021-12-31 19:33:53 +0900

    add lang icons to lovejewerly test

--- src/components/SubComponents/OtherLangIcons.js
+++ src/components/SubComponents/OtherLangIcons.js
@@ -1237,6 +1237,34 @@ export default function OtherLangIcons(props) {
         }
     }
 
+    function lovejewerlyLinkRenderer(){
+        if(props.currentTest === "lovejewerly") {
+            return(
+                <div className="go-to-each-language">
+                    <h3>▼GO TO MY COUNTRY▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/lovejewerlyEng/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img src='https://images.ktestone.com/default/languageIcon/usa-icon.png' className='language-icon' alt="[ Go to the English version ]" /></a>
+                </div>
+            )
+        } else if(props.currentTest === "lovejewerlyEng") {
+            return(
+                <div className="go-to-each-language">
+                    <h3>▼GO TO MY COUNTRY▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/lovejewerly/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img src='https://images.ktestone.com/default/languageIcon/kor-icon.png' className='language-icon' alt="[ 한국어 버전으로 하러가기 ]" /></a>
+                </div>
+            )
+        } 
+    }
+
     return (
         <Fragment>
             {factBTILinkRenderer()}
@@ -1248,6 +1276,7 @@ export default function OtherLangIcons(props) {
             {oneSidedLoveLinkRenderer()}
             {idealTypeLinkRenderer()}
             {fruitAltLinkRenderer()}
+            {lovejewerlyLinkRenderer()}
         </Fragment>
     )
 }
