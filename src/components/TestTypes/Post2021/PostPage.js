63dff0044060b782f6106dde77435ba9653463ee
kapable <skjoldenstore@gmail.com>
2022-08-25 23:44:52 +0900

    add coupang to PostBox

--- src/components/TestTypes/Post2021/PostPage.js
+++ src/components/TestTypes/Post2021/PostPage.js
@@ -7,6 +7,7 @@ import ScriptTag from 'react-script-tag'
 import Pagination from "react-js-pagination";
 import ReactGA from 'react-ga';
 import { Helmet } from 'react-helmet';
+import { useCookies } from 'react-cookie';
 
 function PostPage(props) {
     const lang = props.language;
@@ -28,7 +29,41 @@ function PostPage(props) {
     const [sendMailImg, setSendMailImg] = useState(``);
     const [shareMyPostImg, setShareMyPostImg] = useState(``);
     const [goToMyPostImg, setGoToMyPostImg] = useState(``);
-    const nickNameInput = "https://images.ktestone.com/PostImg/MailForm/nickname-input.png"
+    const nickNameInput = "https://images.ktestone.com/PostImg/MailForm/nickname-input.png";
+
+    // about Coupang Button
+    const [coupangCookies, setCoupangCookie] = useCookies(['coupang']);
+    const [isOpened, setIsOpened] = useState(false);
+    let adProb = 1.1 >= Math.random();
+    const coupangLink = "https://link.coupang.com/a/l7x1e";
+
+    const onCoupangButtonClick = () => {
+        const cookieAges = (24 - new Date().getHours()) <= 12 ? 60*60*(24 - new Date().getHours()) : 60*60*12;
+        _eventSenderGA("Paging", "Click go-to-Coupang Button", "post page");
+        setIsOpened(true);
+        return setCoupangCookie('coupang', true, { path: '/', maxAge: cookieAges, secure: true });
+    }
+
+    const affiliateRenderer = (comment) => (
+        <div className='post-adCover-div-1'>
+            <div className='post-adCover-div-2'>
+                <a href={coupangLink} target="_blank" rel='noreferrer noopener'>
+                    <button className='post-result-coupang-button' type="primary" shape='round' style={{ width: '15rem', height: '3.5rem'}} onClick={onCoupangButtonClick}>
+                        쿠팡 보고 편지 {comment}<br /><p style={{ fontSize: '0.5rem', color: 'lightgray' }}>원치 않을 경우 뒤로 가기를 눌러주세요</p>
+                    </button>
+                </a>
+                <p className='post-result-coupang-comment'>* 이 포스팅은 쿠팡 파트너스 활동의 일환으로,<br />이에 따른 일정액의 수수료를 제공받습니다.</p>
+            </div>
+        </div>
+    )
+    
+    function _eventSenderGA(category, action, label){
+        ReactGA.event({
+            category: category,
+            action: action,
+            label: label
+        });
+    }
 
     const PageBackground = styled.div` 
         background-image: url("https://images.ktestone.com/PostImg/Background/background.png");
@@ -45,14 +80,6 @@ function PostPage(props) {
             display: none;
         }
     `
-
-    function _eventSenderGA(category, action, label){
-        ReactGA.event({
-            category: category,
-            action: action,
-            label: label
-        });
-    }
 
     function adTagRenderer(){
         return(
@@ -232,27 +259,32 @@ function PostPage(props) {
                         <h4 className='post-page-whosname'>{`${userNickname}`} {lang === `Eng` ? `'s` : `님의`}<br></br>POST</h4>
                         <h4 className='post-page-mailcount'>{mailCount}{lang === `Eng` ? ` letters` : `개의 편지`}</h4>
                     </div>
-                    <div className='post-page-btn-div'>
-                        <img src={sendMailImg} alt="Send Mail" className="post-page-send-mail-btn" onClick={() => {
-                            _eventSenderGA("Paging", "Click Post-to-write Button", "post page");
-                            if(lang === `Eng`) {
-                                props.history.push({
-                                    pathname:`/post2022Eng/${props.match.params.username}/postwrite/`,
-                                })
-                            } else {
-                                props.history.push({
-                                    pathname:`/post2021/${props.match.params.username}/postwrite/`,
-                                })
-                            }
-                        }} />
-                        <CopyToClipboard text={`https://ktestone.com/kapable.github.io/post2021/${props.match.params.username}/`}>
-                            <img src={shareMyPostImg} alt="Share My Post" onClick={onShareBtnClick} className='post-page-share-mypost-btn' />
-                        </CopyToClipboard>
-                    </div>             
-                    <div className='post-page-mails-div'>
-                        {mailList.length === 0 ? nullMailRenderer() : mailRenderer(mailList, isLogin)}
-                    </div>
-                    {pageRenderer(page, mailCount)}
+                    {/* Coupang or Mails */}
+                    {adProb && (!isOpened || !coupangCookies)
+                    ? affiliateRenderer('보기')
+                    :<>
+                        <div className='post-page-btn-div'>
+                            <img src={sendMailImg} alt="Send Mail" className="post-page-send-mail-btn" onClick={() => {
+                                _eventSenderGA("Paging", "Click Post-to-write Button", "post page");
+                                if(lang === `Eng`) {
+                                    props.history.push({
+                                        pathname:`/post2022Eng/${props.match.params.username}/postwrite/`,
+                                    })
+                                } else {
+                                    props.history.push({
+                                        pathname:`/post2021/${props.match.params.username}/postwrite/`,
+                                    })
+                                }
+                            }} />
+                            <CopyToClipboard text={`https://ktestone.com/kapable.github.io/post2021/${props.match.params.username}/`}>
+                                <img src={shareMyPostImg} alt="Share My Post" onClick={onShareBtnClick} className='post-page-share-mypost-btn' />
+                            </CopyToClipboard>
+                        </div>             
+                        <div className='post-page-mails-div'>
+                            {mailList.length === 0 ? nullMailRenderer() : mailRenderer(mailList, isLogin)}
+                        </div>
+                        {pageRenderer(page, mailCount)}
+                    </>}
                     <br></br>
                 </PageBackground>
                 <div className="popup-div">
@@ -284,44 +316,50 @@ function PostPage(props) {
                         <h4 className='post-page-whosname'>{`${userNickname}`} {lang === `Eng` ? `'s` : `님의`}<br></br>POST</h4>
                         <h4 className='post-page-mailcount'>{mailCount}{lang === `Eng` ? ` letters` : `개의 편지`}</h4>
                     </div>
-                <div className='post-page-btn-div'>
-                    <img src={sendMailImg} alt="Send Mail" className="post-page-send-mail-btn" onClick={() => {
-                        _eventSenderGA("Paging", "Click Post-to-write Button", "post page");
-                        if(lang === `Eng`) {
-                            props.history.push({
-                                pathname:`/post2022Eng/${props.match.params.username}/postwrite/`,
-                            })
-                        } else {
-                            props.history.push({
-                                pathname:`/post2021/${props.match.params.username}/postwrite/`,
-                            })
-                        }
-                    }} />
-                    <img src={goToMyPostImg} alt="Go to My Post" onClick={() => {
-                        _eventSenderGA("Paging", "Click Post-to-auth Button", "post page");
-                        if(lang === `Eng`) {
-                            props.history.push({
-                                pathname:`/auth/`,
-                                state: {
-                                    language: lang
-                                },
-                            })
-                        } else {
-                            props.history.push({
-                                pathname:`/auth/`,
-                                state: {
-                                    language: lang
-                                },
-                            })
-                        }
-                    }} className='post-page-goto-mypost-btn' />
+                    {/* Coupang or Mails */}
+                    {adProb && (!isOpened || !coupangCookies)
+                    ? affiliateRenderer('보내기')
+                    :<>
+                    <div className='post-page-btn-div'>
+                        <img src={sendMailImg} alt="Send Mail" className="post-page-send-mail-btn" onClick={() => {
+                            _eventSenderGA("Paging", "Click Post-to-write Button", "post page");
+                            if(lang === `Eng`) {
+                                props.history.push({
+                                    pathname:`/post2022Eng/${props.match.params.username}/postwrite/`,
+                                })
+                            } else {
+                                props.history.push({
+                                    pathname:`/post2021/${props.match.params.username}/postwrite/`,
+                                })
+                            }
+                        }} />
+                        <img src={goToMyPostImg} alt="Go to My Post" onClick={() => {
+                            _eventSenderGA("Paging", "Click Post-to-auth Button", "post page");
+                            if(lang === `Eng`) {
+                                props.history.push({
+                                    pathname:`/auth/`,
+                                    state: {
+                                        language: lang
+                                    },
+                                })
+                            } else {
+                                props.history.push({
+                                    pathname:`/auth/`,
+                                    state: {
+                                        language: lang
+                                    },
+                                })
+                            }
+                        }} className='post-page-goto-mypost-btn' />
+                    </div>
+                    <div className='post-page-mails-div'>
+                        {mailList.length === 0 ? nullMailRenderer() : mailRenderer(mailList, isLogin)}
+                    </div>
+                        {pageRenderer(page, mailCount)}
+                        <br></br>
+                    </>}
                 </div>
-                <div className='post-page-mails-div'>
-                    {mailList.length === 0 ? nullMailRenderer() : mailRenderer(mailList, isLogin)}
-                </div>
-                    {pageRenderer(page, mailCount)}
-                    <br></br>
-                </div>
+                
                 {adTagRenderer()}
                 <a
                     target="_blank"
