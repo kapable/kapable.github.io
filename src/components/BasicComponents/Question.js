253cce79671707a196191e55bc66dbbc0f5314a3
kapable <skjoldenstore@gmail.com>
2022-12-25 07:06:33 +0900

    add div block for intro and quiz page

--- src/components/BasicComponents/Question.js
+++ src/components/BasicComponents/Question.js
@@ -4,6 +4,7 @@ class Question extends Component {
     render(){
         return(
             <div className="question">
+                <div id="optadnpm ATB" style={{"minHeight": "220px"}}></div>
                 <h5 className="question-p">{this.props.question}</h5>
             </div>
         );
