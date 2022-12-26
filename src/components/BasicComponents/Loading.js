7645a21fa34ca2155c4a5b7d2b64adf0f1ca4b3a
kapable <skjoldenstore@gmail.com>
2022-12-26 14:58:25 +0900

    fix loading component

--- src/components/BasicComponents/Loading.js
+++ src/components/BasicComponents/Loading.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Fragment } from 'react';
 import Lottie from 'react-lottie';
-import * as loading from '../../loading-animation.json';
+import * as loading from '../../loading-animation.json'
+import ScriptTag from 'react-script-tag'
 
 const defaultOptions = {
     loop: true,
@@ -16,7 +17,9 @@ const defaultOptions = {
 class Loading extends Component {
   componentDidMount(){
     // if condition for Adsense domain
-    if(window) (window.adsbygoogle = window.adsbygoogle || []).push({});
+    if(window.location.href.includes("niair.xyz")){
+        if(window) (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
   }
 
   render(){
@@ -24,6 +27,11 @@ class Loading extends Component {
       if((window.location.href.includes("localhost") || window.location.href.includes("ktestone.com")) && (!ppl_list.includes(this.props.test))) {
           return(
             <Fragment>
+              {/* <ins className="kakao_ad_area" style={{display:"none"}}
+              data-ad-unit    = "DAN-M3XcjSrV4BrUGCJG"
+              data-ad-width   = "300"
+              data-ad-height  = "250"></ins>
+              <ScriptTag type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></ScriptTag> */}
               <div className="loading" >
                   <Lottie options={defaultOptions} height={120} width={120}/>
               </div>
@@ -32,6 +40,11 @@ class Loading extends Component {
         } else if(window.location.href.includes("https://kapable.github.io/")) {
           return(
             <Fragment>
+              <ins className="kakao_ad_area" style={{display:"none"}}
+              data-ad-unit    = "DAN-rgfAOJhp6Faz2JFX"
+              data-ad-width   = "300"
+              data-ad-height  = "250"></ins>
+              <ScriptTag type="text/javascript" src="//t1.daumcdn.net/kas/static/ba.min.js" async></ScriptTag>
               <div className="loading" >
                   <Lottie options={defaultOptions} height={120} width={120}/>
               </div>
@@ -41,6 +54,12 @@ class Loading extends Component {
           return(
             <Fragment>
               {/* 로딩 중앙 */}
+              <ins className="adsbygoogle"
+                  style={{display:"block"}}
+                  data-ad-client="ca-pub-2382342018701919"
+                  data-ad-slot="4650135764"
+                  data-ad-format="auto"
+                  data-full-width-responsive="true"></ins>
               <div className="loading" >
                   <Lottie options={defaultOptions} height={120} width={120}/>
               </div>
