7c89acc90412b2b0394a2e6144157c5a05df41a8
kapable <skjoldenstore@gmail.com>
2022-12-26 13:48:29 +0900

    refix for  optAd

--- src/components/BasicComponents/Question.js
+++ src/components/BasicComponents/Question.js
@@ -4,7 +4,7 @@ class Question extends Component {
     render(){
         return(
             <div className="question">
-                {/* <div id="optadnpm ATB" style={{"minHeight": "220px"}}></div> */}
+                <div id="optadnpm ATB" style={{"minHeight": "220px"}}></div>
                 <h5 className="question-p">{this.props.question}</h5>
             </div>
         );
