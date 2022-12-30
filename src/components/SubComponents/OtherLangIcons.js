c953b4193c00cca4102188365981fcc016c6b314
kapable <skjoldenstore@gmail.com>
2022-12-30 19:32:51 +0900

    add other lang icons for fishShapedBunTest

--- src/components/SubComponents/OtherLangIcons.js
+++ src/components/SubComponents/OtherLangIcons.js
@@ -1480,6 +1480,34 @@ export default function OtherLangIcons(props) {
         }
     };
 
+    function fishShapedBunTestLinkRenderer(){
+        if(props.currentTest === "fishShapedBun") {
+            return(
+                <div className="go-to-each-language">
+                    <h3>▼GO TO MY COUNTRY▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/fishShapedBunEng/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img src='https://images.ktestone.com/default/languageIcon/usa-icon.png' className='language-icon' alt="[ Go to the English version ]" /></a>
+                </div>
+            )
+        } else if(props.currentTest === "fishShapedBunEng") {
+            return(
+                <div className="go-to-each-language">
+                    <h3>▼GO TO MY COUNTRY▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/fishShapedBun/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img src='https://images.ktestone.com/default/languageIcon/kor-icon.png' className='language-icon' alt="[ 한국어 버전으로 하러가기 ]" /></a>
+                </div>
+            )
+        }
+    };
+
     return (
         <Fragment>
             {factBTILinkRenderer()}
@@ -1496,6 +1524,7 @@ export default function OtherLangIcons(props) {
             {flowerBTILinkRenderer()}
             {snowflakeLinkRenderer()}
             {smileColorTestLinkRenderer()}
+            {fishShapedBunTestLinkRenderer()}
         </Fragment>
     );
 };
