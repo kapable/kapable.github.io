a18cc0ec2a4de8ba8ec7e47588d531b52c62ba0d
kapable <skjoldenstore@gmail.com>
2023-01-16 17:46:35 +0900

    set full logic for todayLuck in saju

--- src/components/TestTypes/Saju/TodayLuck.js
+++ src/components/TestTypes/Saju/TodayLuck.js
@@ -1,36 +1,38 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import axios from 'axios';
 import { DatePicker } from 'antd';
-// import 'antd/dist/antd.css'; 
+import 'antd/dist/antd.css';
+import moment from 'moment';
 
 const TodayLuck = () => {
-    const [year, setYear] = useState("");
-    const [month, setMonth] = useState("");
+    const dateFormat = 'YYYYMMDD';
     const [day, setDay] = useState("");
     const [result, setResult] = useState("");
     const saju_url = 'https://saju.ktestone.com';
-    useEffect(() => {
-        const getToday = async () => {
-            await axios.get(
-                saju_url + '/today/total' + '/19880711'
-            )
-            .then((res) => {
-                console.log(res);
-                setResult(res?.data?.result?.DB_data)
-            });
-        }
-        getToday();
-    }, [saju_url]);
+
+    const getToday = useCallback(async () => {
+        await axios.get(
+            saju_url + '/today/total/' + day
+        )
+        .then((res) => {
+            console.log(res);
+            setResult(res?.data?.result?.DB_data)
+        });
+    }, [day]);
 
     const onChange = useCallback((date) => {
-        console.log(date);
-    }, []);
+        setDay(moment(date).format(dateFormat));
+    }, [dateFormat]);
 
 
     return (
         <div>
             <h1>태어난 연도와 월을 입력해주세요</h1>
-            <DatePicker onChange={onChange} />
+            <DatePicker onChange={onChange} allowClear/>
+            <div>---</div>
+            <div>
+                <button onClick={getToday}>확인하기</button>
+            </div>
             <div>{result ? result : null}</div>
         </div>
     );
