4656a5c41034b50ea4cdd246c9c543ab0f05e68c
kapable <skjoldenstore@gmail.com>
2023-01-17 13:10:32 +0900

    setting for all luck in todayLuck saju

--- src/components/TestTypes/Saju/TodayLuck.js
+++ src/components/TestTypes/Saju/TodayLuck.js
@@ -1,13 +1,17 @@
 import React, { useCallback, useState } from 'react';
 import axios from 'axios';
-import { DatePicker } from 'antd';
+import { DatePicker, Divider } from 'antd';
 import 'antd/dist/antd.css';
 import moment from 'moment';
+import 'moment/locale/ko';
+import locale from 'antd/es/date-picker/locale/ko_KR';
 
 const TodayLuck = () => {
     const dateFormat = 'YYYYMMDD';
     const [day, setDay] = useState("");
-    const [result, setResult] = useState("");
+    const [result, setResult] = useState({});
+
+
     const saju_url = 'https://saju.ktestone.com';
 
     const getToday = useCallback(async () => {
@@ -15,8 +19,7 @@ const TodayLuck = () => {
             saju_url + '/today/total/' + day
         )
         .then((res) => {
-            console.log(res);
-            setResult(res?.data?.result?.DB_data)
+            setResult(res?.data)
         });
     }, [day]);
 
@@ -28,12 +31,31 @@ const TodayLuck = () => {
     return (
         <div>
             <h1>태어난 연도와 월을 입력해주세요</h1>
-            <DatePicker onChange={onChange} allowClear/>
+            <DatePicker onChange={onChange} allowClear locale={locale}/>
             <div>---</div>
             <div>
                 <button onClick={getToday}>확인하기</button>
             </div>
-            <div>{result ? result : null}</div>
+            <Divider />
+            <div>
+                <h3>오늘의 총운</h3>
+                <p>{result ? result?.total_result : null}</p>
+                <Divider />
+                <h3>오늘의 애정운</h3>
+                <p>{result ? result?.love_result : null}</p>
+                <Divider />
+                <h3>오늘의 소망운</h3>
+                <p>{result ? result?.wish_result : null}</p>
+                <Divider />
+                <h3>오늘의 사업운</h3>
+                <p>{result ? result?.biz_result : null}</p>
+                <Divider />
+                <h3>오늘의 방위운</h3>
+                <p>{result ? result?.direction_result : null}</p>
+                <Divider />
+                <h3>오늘의 금전운</h3>
+                <p>{result ? result?.wealth_result : null}</p>
+            </div>
         </div>
     );
 };
