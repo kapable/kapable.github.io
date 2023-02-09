d281cabe4baab2ba68cf8f28b5640924552e5bff
kapable <skjoldenstore@gmail.com>
2023-02-09 15:01:46 +0900

    init LifetimeSaju component

---
+++ src/components/TestTypes/Saju/LifetimeSaju.js
@@ -0,0 +1,89 @@
+import React, { Fragment, useCallback, useState } from 'react';
+import { DatePicker } from 'antd';
+import moment from 'moment';
+import 'moment/locale/ko';
+import locale from 'antd/es/date-picker/locale/ko_KR';
+import { withRouter } from 'react-router';
+import crypto from 'crypto-js';
+import Lottie from 'react-lottie';
+import * as loading from '../../../loading-animation.json';
+import './TodayLuck.css';
+import { Helmet } from 'react-helmet';
+
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: loading.default,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+};
+
+const TodayLuck = (props) => {
+    const dateFormat = 'YYYYMMDD';
+    const [day, setDay] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onSubmitClick = useCallback(async () => {
+        if(!day) {
+            return alert('날짜를 입력해주세요!');
+        };
+        const today_date = moment().format(dateFormat);
+        const query_date = [today_date, day].join('-');
+        const crypto_query_date = encodeURIComponent(crypto.AES.encrypt(JSON.stringify(query_date), 'ktestsaju').toString());
+        
+        setIsLoading(true);
+        setTimeout(() => {
+            props.history.push(`/todayLuck/${crypto_query_date}/`)
+        }, "2500");
+    }, [day, props]);
+
+    const onChange = useCallback((date) => {
+        setDay(moment(date).format(dateFormat));
+    }, [dateFormat]);
+
+    if(isLoading) {
+        return (
+            <Lottie options={defaultOptions} height={120} width={120} style={{ marginTop: "10rem" }} />
+        );
+    } else {
+        return (
+            <Fragment>
+                <Helmet>
+                    {/* <!-- Primary Meta Tags --> */}
+                    <title>오늘의 총운 - 케이테스트 | 사주 테스트</title>
+                    <meta name="title" content="오늘의 총운 - 케이테스트"/>
+                    <meta name="description" content="오늘 하루 나의 총운은? - 오늘의 총운 | 케이테스트 | 사주 테스트" data-react-helmet="true"/>
+                    <link rel="main-url" href="https://ktestone.com/kapable.github.io/todayLuck/"/>
+
+                    {/* <!-- Open Graph / Facebook --> */}
+                    <meta property="og:type" content="website"/>
+                    <meta property="og:url" content="https://ktestone.com/kapable.github.io/todayLuck/"/>
+                    <meta property="og:title" content="오늘의 총운 - 케이테스트 | 사주 테스트"/>
+                    <meta property="og:description" content="오늘 하루 나의 총운은? - 오늘의 총운 | 케이테스트 | 사주 테스트"/>
+                    <meta property="og:image" content={`https://images.ktestone.com/main-thumbnail/todayLuck-thumb.png`}/>
+                    <meta property="og:image:alt" content="오늘의 총운 - 케이테스트 | 사주 테스트" />
+
+                    {/* <!-- Twitter --> */}
+                    <meta property="twitter:card" content="summary_large_image"/>
+                    <meta property="twitter:url" content="https://ktestone.com/kapable.github.io/todayLuck/"/>
+                    <meta property="twitter:title" content="오늘의 총운 - 케이테스트 | 사주 테스트"/>
+                    <meta property="twitter:description" content="오늘 하루 나의 총운은? - 오늘의 총운 | 케이테스트 | 사주 테스트"/>
+                    <meta property="twitter:image" content={`https://images.ktestone.com/main-thumbnail/todayLuck-thumb.png`}/>
+                    <meta property="twitter:image:alt" content="오늘의 총운 - 케이테스트 | 사주 테스트" />
+                </Helmet>
+                <div className='todayLuck-intro-img-div'>
+                    <img className='todayLuck-intro-bg' src="https://images.ktestone.com/meta/saju/todayLuck-intro-bg.jpg" alt="todayLuck-intro-input"/>
+                    <div className='todayLuck-intro-date-picker-div'>
+                        <DatePicker className='todayLuck-intro-date-picker' onChange={onChange} allowClear locale={locale}/>
+                    </div>
+                    <div className='todayLuck-intro-btn-div' onClick={onSubmitClick}>
+                        <img className='todayLuck-intro-btn' src="https://images.ktestone.com/meta/saju/todayLuck-intro-submit-btn.jpg" alt="todayLuck-intro-btn"/>
+                    </div>
+                </div>
+            </Fragment>
+        );
+    };
+};
+
+export default withRouter(TodayLuck);
