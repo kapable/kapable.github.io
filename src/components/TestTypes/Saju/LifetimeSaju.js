0a06d24628429afd8a7d97c9336a2a7ae79f62bd
kapable <skjoldenstore@gmail.com>
2023-02-14 16:48:22 +0900

    change meta tag for lifetime saju

--- src/components/TestTypes/Saju/LifetimeSaju.js
+++ src/components/TestTypes/Saju/LifetimeSaju.js
@@ -69,7 +69,7 @@ const LifetimeSaju = (props) => {
     const onShareButtonClick = useCallback(() => {
         alert("링크가 복사됐습니다!");
         _eventSenderGA("Sharing", "Click Copy-link Button", "intro page");
-    }, [])
+    }, []);
 
     if(isLoading) {
         return (
@@ -80,26 +80,26 @@ const LifetimeSaju = (props) => {
             <Fragment>
                 <Helmet>
                     {/* <!-- Primary Meta Tags --> */}
-                    <title>평생 총운 - 케이테스트 | 사주 테스트</title>
-                    <meta name="title" content="평생 총운 - 케이테스트"/>
-                    <meta name="description" content="나의 평생 총운은? - 자평명리학 | 평생 총운 | 케이테스트 | 사주 테스트" data-react-helmet="true"/>
+                    <title>사주 총평 당신의 사주는? - 케이테스트 | 사주 테스트</title>
+                    <meta name="title" content="사주 총평 당신의 사주는? - 케이테스트"/>
+                    <meta name="description" content="무료 사주를 케이테스트에서 제공합니다. | 나의 평생 총운은? - 자평명리학 | 평생 총운 | 케이테스트 | 사주 테스트" data-react-helmet="true"/>
                     <link rel="main-url" href="https://ktestone.com/kapable.github.io/lifetimeSaju/"/>
 
                     {/* <!-- Open Graph / Facebook --> */}
                     <meta property="og:type" content="website"/>
                     <meta property="og:url" content="https://ktestone.com/kapable.github.io/lifetimeSaju/"/>
-                    <meta property="og:title" content="평생 총운 - 케이테스트 | 사주 테스트"/>
-                    <meta property="og:description" content="나의 평생 총운은? - 자평명리학 | 평생 총운 | 케이테스트 | 사주 테스트"/>
+                    <meta property="og:title" content="사주 총평 당신의 사주는? - 케이테스트 | 사주 테스트"/>
+                    <meta property="og:description" content="무료 사주를 케이테스트에서 제공합니다. | 나의 평생 총운은? - 자평명리학 | 평생 총운 | 케이테스트 | 사주 테스트"/>
                     <meta property="og:image" content={`https://images.ktestone.com/main-thumbnail/lifetimeSaju-thumb.png`}/>
-                    <meta property="og:image:alt" content="평생 총운 - 케이테스트 | 사주 테스트" />
+                    <meta property="og:image:alt" content="사주 총평 당신의 사주는? - 케이테스트 | 사주 테스트" />
 
                     {/* <!-- Twitter --> */}
                     <meta property="twitter:card" content="summary_large_image"/>
                     <meta property="twitter:url" content="https://ktestone.com/kapable.github.io/lifetimeSaju/"/>
-                    <meta property="twitter:title" content="평생 총운 - 케이테스트 | 사주 테스트"/>
-                    <meta property="twitter:description" content="나의 평생 총운은? - 자평명리학 | 평생 총운 | 케이테스트 | 사주 테스트"/>
+                    <meta property="twitter:title" content="사주 총평 당신의 사주는? - 케이테스트 | 사주 테스트"/>
+                    <meta property="twitter:description" content="무료 사주를 케이테스트에서 제공합니다. | 나의 평생 총운은? - 자평명리학 | 평생 총운 | 케이테스트 | 사주 테스트"/>
                     <meta property="twitter:image" content={`https://images.ktestone.com/main-thumbnail/lifetimeSaju-thumb.png`}/>
-                    <meta property="twitter:image:alt" content="평생 총운 - 케이테스트 | 사주 테스트" />
+                    <meta property="twitter:image:alt" content="사주 총평 당신의 사주는? - 케이테스트 | 사주 테스트" />
                 </Helmet>
                 <div className='lifetime-saju-intro-img-div'>
                     <img className='lifetime-saju-intro-bg' src="https://images.ktestone.com/meta/saju/lifetimeSaju-intro-input.jpg" alt="lifetime-saju-intro-input"/>
@@ -114,21 +114,21 @@ const LifetimeSaju = (props) => {
                     </div>
                 </div>
                 <div className="test-intro-with-friend">
-                        <CopyToClipboard text={'https://ktestone.com/kapable.github.io/lifetimeSaju/'}>
-                            <img
-                                src={COPYBTN}
-                                className="test-intro-with-friend-img"
-                                onClick={onShareButtonClick}
-                                alt="테스트 링크 복사"/>
-                        </CopyToClipboard>
-                    </div>
-                    <div className="test-intro-to-main">
+                    <CopyToClipboard text={'https://ktestone.com/kapable.github.io/lifetimeSaju/'}>
                         <img
-                            className="test-intro-to-main-img"
-                            src={BACKBTN}
-                            onClick={onBacktoHomeButtonClick}
-                            alt="다른 테스트 하러 뒤로가기"/>
-                    </div>
+                            src={COPYBTN}
+                            className="test-intro-with-friend-img"
+                            onClick={onShareButtonClick}
+                            alt="테스트 링크 복사"/>
+                    </CopyToClipboard>
+                </div>
+                <div className="test-intro-to-main">
+                    <img
+                        className="test-intro-to-main-img"
+                        src={BACKBTN}
+                        onClick={onBacktoHomeButtonClick}
+                        alt="다른 테스트 하러 뒤로가기"/>
+                </div>
             </Fragment>
         );
     };
