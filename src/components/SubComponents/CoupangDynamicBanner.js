d9e2f52fc7563990bdfd016d2d3f090336c54894
kapable <skjoldenstore@gmail.com>
2023-02-24 18:27:53 +0900

    add GA event label for coupang dynamic banner click event

--- src/components/SubComponents/CoupangDynamicBanner.js
+++ src/components/SubComponents/CoupangDynamicBanner.js
@@ -1,9 +1,21 @@
 import React, { Fragment } from 'react';
+import ReactGA from 'react-ga';
 
 const CoupangDynamicBanner = () => {
+    const _eventSenderGA = (category, action, label) => {
+        ReactGA.event({
+            category: category,
+            action: action,
+            label: label
+        });
+    };
+
     return (
         <Fragment>
-            <iframe title='ads-partners-coupang' src="https://ads-partners.coupang.com/widgets.html?id=643134&template=carousel&trackingCode=AF4396324&subId=&width=300&height=300" width="300" height="300" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"></iframe>
+            <iframe
+                title='ads-partners-coupang' src="https://ads-partners.coupang.com/widgets.html?id=643134&template=carousel&trackingCode=AF4396324&subId=&width=300&height=300" width="300" height="300" frameborder="0" scrolling="no" referrerpolicy="unsafe-url"
+                onClick={() => _eventSenderGA("Clicking", "Click Coupang Dynamic Banner", "result page")}
+            ></iframe>
         </Fragment>
     );
 };
