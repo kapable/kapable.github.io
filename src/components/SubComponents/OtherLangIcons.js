8ff1f0a5fe7b5cf277e715604c482fc9860ba4a3
kapable <skjoldenstore@gmail.com>
2023-03-09 19:07:57 +0900

    add otherLangIcons and fix routes for coupleCharactertests

--- src/components/SubComponents/OtherLangIcons.js
+++ src/components/SubComponents/OtherLangIcons.js
@@ -2512,6 +2512,64 @@ export default function OtherLangIcons(props) {
         }
     };
 
+    function coupleCharacterTestLinkRenderer(){
+        if(["coupleCharacter", "coupleCharacterFemale", "coupleCharacterMale"].includes(props.currentTest)) {
+            return(
+                <div className="go-to-each-language" style={{marginTop:'1rem'}}>
+                    <h3>▼GO TO MY COUNTRY▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/coupleCharacterEng/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img src='https://images.ktestone.com/default/languageIcon/usa-icon.png' className='language-icon' alt="[ Go to the English version ]" /></a>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/coupleCharacterCN/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img src='https://images.ktestone.com/default/languageIcon/china-icon.png' className='language-icon' alt="[ 汉语版本试 ]" /></a>
+                </div>
+            )
+        } else if(["coupleCharacterEng", "coupleCharacterFemaleEng", "coupleCharacterMaleEng"].includes(props.currentTest)) {
+            return(
+                <div className="go-to-each-language" style={{marginTop:'1rem'}}>
+                    <h3>▼GO TO MY COUNTRY▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/coupleCharacter/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img src='https://images.ktestone.com/default/languageIcon/kor-icon.png' className='language-icon' alt="[ 한국어 버전으로 하러가기 ]" /></a>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/coupleCharacterCN/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img src='https://images.ktestone.com/default/languageIcon/china-icon.png' className='language-icon' alt="[ 汉语版本试 ]" /></a>
+                </div>
+            )
+        } else if(["coupleCharacterCN", "coupleCharacterFemaleCN", "coupleCharacterMaleCN"].includes(props.currentTest)) {
+            return(
+                <div className="go-to-each-language" style={{marginTop:'1rem'}}>
+                    <h3>▼GO TO MY COUNTRY▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/coupleCharacter/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img src='https://images.ktestone.com/default/languageIcon/kor-icon.png' className='language-icon' alt="[ 한국어 버전으로 하러가기 ]" /></a>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/coupleCharacterEng/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img src='https://images.ktestone.com/default/languageIcon/usa-icon.png' className='language-icon' alt="[ Go to the English version ]" /></a>
+                </div>
+            )
+        } 
+    };
+
     return (
         <Fragment>
             {factBTILinkRenderer()}
@@ -2540,6 +2598,7 @@ export default function OtherLangIcons(props) {
             {loveFlameTestLinkRenderer()}
             {bondeeTestLinkRenderer()}
             {speechHabitTestLinkRenderer()}
+            {coupleCharacterTestLinkRenderer()}
         </Fragment>
     );
 };
