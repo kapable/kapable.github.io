a03b7cbacf240fc05cfb7a7f2df18954e1250634
kapable <skjoldenstore@gmail.com>
2023-04-12 13:30:01 +0900

    fix minor grammar

--- src/components/BasicComponents/Result.js
+++ src/components/BasicComponents/Result.js
@@ -55,7 +55,7 @@ class Result extends Component {
             ppl_list:['auditionBTI', 'auditionBTIEng', 'auditionBTIJp', 'auditionBTICn', 'personalIncense', 'personalTaro', 'jaetech', 'wealthluck'],
             coupangCookies: cookies.get('coupang') || null,
             isOpened: false,
-            originAdProb: 0.1 < Math.random(),
+            originAdProb: 0.3 < Math.random(),
             adProb: 1.1 >= Math.random(),
         };
         this._onBackToStartButtonClick = this._onBackToStartButtonClick.bind(this);
