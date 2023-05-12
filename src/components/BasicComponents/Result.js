e5ad8a6ce45d0b08053629a2c90c29cba2480af5
kapable <skjoldenstore@gmail.com>
2023-05-12 18:10:01 +0900

    change coupang partners link

--- src/components/BasicComponents/Result.js
+++ src/components/BasicComponents/Result.js
@@ -797,7 +797,7 @@ class Result extends Component {
     };
 
     affiliateRenderer(){
-        const cookieRocketCoupangLink = this.state.originAdProb ? "https://link.coupang.com/a/KgpKa" : "https://link.coupang.com/a/LgvP2";
+        const cookieRocketCoupangLink = this.state.originAdProb ? "https://link.coupang.com/a/X2X8X" : "https://link.coupang.com/a/X2Zw2";
         let otherAdProb = 0.2 >= Math.random();
         const othersLink = [
             { test: "personalScentBTI", coupangLink: "https://link.coupang.com/a/FFVJ2" },
