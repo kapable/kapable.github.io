5e76b05c139e8d1e26c3b51beef99f9f0d780cb1
kapable <skjoldenstore@gmail.com>
2023-05-13 15:22:04 +0900

    temporarly pausing coupang banner in Loading page

--- src/components/BasicComponents/Loading.js
+++ src/components/BasicComponents/Loading.js
@@ -3,7 +3,7 @@ import { Fragment } from 'react';
 import Lottie from 'react-lottie';
 import * as loading from '../../loading-animation.json'
 import ScriptTag from 'react-script-tag'
-import CoupangDynamicBanner from '../SubComponents/CoupangDynamicBanner';
+// import CoupangDynamicBanner from '../SubComponents/CoupangDynamicBanner';
 
 const defaultOptions = {
     loop: true,
@@ -22,7 +22,7 @@ class Loading extends Component {
       if((window.location.href.includes("localhost") || window.location.href.includes("ktestone.com")) && (!ppl_list.includes(this.props.test))) {
           return(
             <Fragment>
-              {this.props.lang === 'Kor' && !ppl_list.includes(this.props.test) ? <CoupangDynamicBanner page={'loading'} /> : null}
+              {/* {this.props.lang === 'Kor' && !ppl_list.includes(this.props.test) ? <CoupangDynamicBanner page={'loading'} /> : null} */}
               <div className="loading" >
                   <Lottie options={defaultOptions} height={120} width={120}/>
               </div>
@@ -31,7 +31,7 @@ class Loading extends Component {
         } else if(window.location.href.includes("https://kapable.github.io/")) {
           return(
             <Fragment>
-              {this.props.lang === 'Kor' && !ppl_list.includes(this.props.test) ? <CoupangDynamicBanner page={'loading'} /> : null}
+              {/* {this.props.lang === 'Kor' && !ppl_list.includes(this.props.test) ? <CoupangDynamicBanner page={'loading'} /> : null} */}
               <ins className="kakao_ad_area" style={{display:"none"}}
               data-ad-unit    = "DAN-rgfAOJhp6Faz2JFX"
               data-ad-width   = "300"
