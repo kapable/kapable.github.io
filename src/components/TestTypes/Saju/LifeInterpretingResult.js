e19c9a69b0b1485a7ac30fa6192a43b9d54fb59a
kapable <skjoldenstore@gmail.com>
2023-05-20 23:21:25 +0900

    apply new coupang button design for SAJU contents

--- src/components/TestTypes/Saju/LifeInterpretingResult.js
+++ src/components/TestTypes/Saju/LifeInterpretingResult.js
@@ -108,28 +108,25 @@ const LifetimeSajuResult = (props) => {
                     <div className='article-adCover-div-1'>
                         <div className='article-adCover-div-2'>
                             <div className='article-adCover-div-3'>
+                                <p><b><span style={{color:"#4185F4"}}>콘텐츠를 보기 전</span> 쿠팡 쇼핑을 해보세요</b></p>
                                 <a href={coupangLink} target="_blank" rel='noreferrer noopener'>
                                     <button className='result-coupang-button' type="primary" shape='round' style={{ width: '15rem', height: '3.5rem'}} onClick={onCoupangButtonClick}>
-                                        쿠팡 보고 결과 보기<br /><p style={{ fontSize: '0.5rem', color: 'lightgray' }}>원치 않을 경우 뒤로 가기를 눌러주세요</p>
+                                        버튼 누르고 계속 보기
                                     </button>
                                 </a>
                                 {startTimer ? (
-                                    <button className='coupang-close-button'
-                                        onClick={coupangCount === 0 ? onCoupangCloseButtonClick : null}>
-                                        {
-                                            
-                                            coupangCount === 0 ? "X" : coupangCount
-                                        }
-                                    </button>
+                                    <p style={{ fontSize: '10px', color: 'grey', marginTop: "1em" }}>원치 않을 경우 {  
+                                        coupangCount === 0 ? <span style={{color:'red', cursor:'pointer'}} onClick={coupangCount === 0 ? onCoupangCloseButtonClick : null}>X</span> : coupangCount
+                                        }를 눌러주세요</p>
                                 ) : null}
                             </div>
+                            <p className='result-coupang-comment' style={{marginTop: "1rem"}}>* 이 포스팅은 쿠팡 파트너스 활동의 일환으로,<br />이에 따른 일정액의 수수료를 제공받습니다.</p>
                         </div>
-                        <p className='result-coupang-comment' style={{marginTop: "1rem"}}>* 이 포스팅은 쿠팡 파트너스 활동의 일환으로,<br />이에 따른 일정액의 수수료를 제공받습니다.</p>
                     </div>
                 </Fragment>
             }
             <div className="share">
-                <h5 className="share-title">{"친구에게 공유하기"}</h5>
+                <h5 className="share-title share-title-saju">친구에게 공유하기</h5>
                 <ShareGroup
                     link={"https://ktestone.com/kapable.github.io/lifeInterpreting/"}
                     testTitle={"인생 풀이 | 내 인생운은 어떨까? - 케이테스트 | 사주 테스트"}/>
