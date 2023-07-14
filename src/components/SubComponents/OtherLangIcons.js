f1d1b29507b321f50a21ffe29a7b8beb8c502fc9
kapable <skjoldenstore@gmail.com>
2023-07-14 14:24:34 +0900

    add otherLangIcons for digitalCharTest

--- src/components/SubComponents/OtherLangIcons.js
+++ src/components/SubComponents/OtherLangIcons.js
@@ -4156,6 +4156,34 @@ export default function OtherLangIcons(props) {
         }
     };
 
+    function digitalCharTestLinkRenderer(){
+        if(props.currentTest === "digitalChar") {
+            return(
+                <div className="go-to-each-language">
+                    <h3>▼OTHER LANGUAGES▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/digitalCharEng/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img loading="lazy" src='https://images.ktestone.com/default/languageIcon/usa-icon.png' className='language-icon' alt="[ Go to the English version ]" /></a>
+                </div>
+            )
+        } else if(props.currentTest === "digitalCharEng") {
+            return(
+                <div className="go-to-each-language">
+                    <h3>▼OTHER LANGUAGES▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/digitalChar/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img loading="lazy" src='https://images.ktestone.com/default/languageIcon/kor-icon.png' className='language-icon' alt="[ 한국어 버전으로 하러가기 ]" /></a>
+                </div>
+            )
+        }
+    };
+
     return (
         <Fragment>
             {factBTILinkRenderer()}
@@ -4205,6 +4233,7 @@ export default function OtherLangIcons(props) {
             {mazeHeartTestLinkRenderer()}
             {postCardTestLinkRenderer()}
             {flowerGardenTestLinkRenderer()}
+            {digitalCharTestLinkRenderer()}
         </Fragment>
     );
 };
