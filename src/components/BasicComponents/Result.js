b73968ee58122fad1fc52349890afbcd599f9f82
kapable <skjoldenstore@gmail.com>
2023-08-30 15:36:47 +0900

    change coupang button design to original

--- src/components/BasicComponents/Result.js
+++ src/components/BasicComponents/Result.js
@@ -888,16 +888,16 @@ class Result extends Component {
             <div className='article-adCover-div-1'>
                 <div className='article-adCover-div-2'>
                     <div className='article-adCover-div-3'>
-                        <p style={{ margin: 0 }}><b><span style={{color:"#4185F4"}}>지금 쿠팡 방문하고</span></b></p>
-                        <div><p style={{ fontSize: '1.4rem', fontWeight: "bolder" }}>2시간동안 광고 제거하기!</p></div>
-                        <p style={{ fontSize: "0.7rem", width: "0.7rem", color: "white", backgroundColor: "#83b4de", position:"absolute", right:"0", top: "6rem" }}>
+                        <p><b><span style={{color:"#4185F4"}}>콘텐츠를 보기 전</span> 쿠팡 쇼핑을 해보세요</b></p>
+                        <div><p style={{ fontSize: '0.7rem' }}>쿠팡 방문은 케이테스트가 항상 질좋은 콘텐츠를 제공 할 수 있는 힘이 됩니다.<br />항상 케이테스트 콘텐츠를 사랑해주셔서 감사합니다.</p></div>
+                        <p style={{ fontSize: "0.7rem", width: "0.7rem", color: "white", backgroundColor: "#83b4de", position:"absolute", right:"4rem", top: "5.2rem" }}>
                             {this.state.startTimer ? (
                                     this.state.coupangCount === 0 ? <span style={{color:'white', cursor:'pointer'}} onClick={this.state.coupangCount === 0 ? this.onCoupangCloseButtonClick : null}><b>X</b></span> : this.state.coupangCount
                             ) :null}
                         </p>
                         <a href={testsArray.includes(this.state.current_test) && otherAdProb ? othersLink.find((item) => item?.test === this.state.current_test)?.coupangLink : cookieRocketCoupangLink} target="_blank" rel='noreferrer noopener'>
                             <button className='result-coupang-button' type="primary" shape='round' style={{ width: '15rem', height: '3.5rem'}} onClick={testsArray.includes(this.state.current_test) && otherAdProb ? this.onOtherCoupangButtonClick : () => this.onCoupangButtonClick(this.state.current_test)}>
-                                쿠팡 방문하고 결과보기
+                                버튼 누르고 결과 보기
                             </button>
                         </a>
                         <p style={{ fontSize: '10px', color: 'grey', marginTop: "0.5rem" }}>원치 않을 경우 뒤로 가기를 눌러주세요.</p>
