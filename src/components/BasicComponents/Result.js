3d132487fd8d8c8cb90418b65a4d93182c66e2fd
kapable <skjoldenstore@gmail.com>
2023-09-09 17:17:03 +0900

    add puffyClouds test

--- src/components/BasicComponents/Result.js
+++ src/components/BasicComponents/Result.js
@@ -417,7 +417,7 @@ class Result extends Component {
         let percentageMBTI2022ListKor = ["percentageMBTI2022",];
         let percentageMBTI2022ListOverseas = ["percentageMBTI2022Eng","percentageMBTI2022Cn"];
         let egoProfileEngList = ["egoProfileEng"];
-        let guestbookCharList = ["guestbookChar"];
+        let puffyCloudsList = ["puffyClouds"];
         let hmallList = ['gardenflower', 'hmall', 'labelStickerSKT'];
         if(personalColor2022ListKor.includes(this.state.current_test)){
             const jelling_outlink = "https://bit.ly/3FlwKMJ";
@@ -539,8 +539,8 @@ class Result extends Component {
                     ))}
                 </Fragment>
             )
-        } else if (guestbookCharList.includes(this.state.current_test)) {
-            const rederTestList = ["bookChar", "newsChar", "zgChar", "remindProfile", "iPhoneMemo",];
+        } else if (puffyCloudsList.includes(this.state.current_test)) {
+            const rederTestList = ["guestbookChar", "bookChar", "newsChar", "zgChar", "remindProfile", ];
             return (
                 <Fragment>
                     {rederTestList.map((test) => (
@@ -806,7 +806,7 @@ class Result extends Component {
         } else if (hmallList.includes(this.state.current_test)) {
             return null;
         } else {
-            const rederTestList = ["guestbookChar", "bookChar", "newsChar", "zgChar", "remindProfile", ];
+            const rederTestList = ["puffyClouds", "guestbookChar", "bookChar", "newsChar", "zgChar",];
             return(
                 <Fragment>
                     {rederTestList.map((test) => (
