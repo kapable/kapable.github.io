530565ece434581e995a36c11fa365ff431e1acb
kapable <skjoldenstore@gmail.com>
2023-09-20 11:30:42 +0900

    add colorCombo test

--- src/components/BasicComponents/Result.js
+++ src/components/BasicComponents/Result.js
@@ -417,7 +417,7 @@ class Result extends Component {
         let percentageMBTI2022ListKor = ["percentageMBTI2022",];
         let percentageMBTI2022ListOverseas = ["percentageMBTI2022Eng","percentageMBTI2022Cn"];
         let egoProfileEngList = ["egoProfileEng"];
-        let meOtherList = ["meOther"];
+        let colorComboList = ["colorCombo"];
         let hmallList = ['gardenflower', 'hmall', 'labelStickerSKT'];
         if(personalColor2022ListKor.includes(this.state.current_test)){
             const jelling_outlink = "https://bit.ly/3FlwKMJ";
@@ -539,8 +539,8 @@ class Result extends Component {
                     ))}
                 </Fragment>
             )
-        } else if (meOtherList.includes(this.state.current_test)) {
-            const rederTestList = ["neonsignChar", "stoneChar", "lovePotion", "puffyClouds", "guestbookChar",];
+        } else if (colorComboList.includes(this.state.current_test)) {
+            const rederTestList = ["meOther", "neonsignChar", "stoneChar", "lovePotion", "puffyClouds", ];
             return (
                 <Fragment>
                     {rederTestList.map((test) => (
@@ -806,7 +806,7 @@ class Result extends Component {
         } else if (hmallList.includes(this.state.current_test)) {
             return null;
         } else {
-            const rederTestList = ["meOther", "neonsignChar", "stoneChar", "lovePotion", "puffyClouds",];
+            const rederTestList = ["colorCombo", "meOther", "neonsignChar", "stoneChar", "lovePotion",];
             return(
                 <Fragment>
                     {rederTestList.map((test) => (
