f11722f81e2cd174f1b5a2944761b5c2322b5b81
kapable <skjoldenstore@gmail.com>
2023-10-14 18:19:46 +0900

    add loveSimulationList test

--- src/components/BasicComponents/Result.js
+++ src/components/BasicComponents/Result.js
@@ -417,7 +417,7 @@ class Result extends Component {
         let percentageMBTI2022ListKor = ["percentageMBTI2022",];
         let percentageMBTI2022ListOverseas = ["percentageMBTI2022Eng","percentageMBTI2022Cn"];
         let egoProfileEngList = ["egoProfileEng"];
-        let highteenStickerList = ["highteenSticker"];
+        let loveSimulationList = ["loveSimulation"];
         let hmallList = ['gardenflower', 'hmall', 'labelStickerSKT'];
         if(personalColor2022ListKor.includes(this.state.current_test)){
             const jelling_outlink = "https://bit.ly/3FlwKMJ";
@@ -539,8 +539,8 @@ class Result extends Component {
                     ))}
                 </Fragment>
             )
-        } else if (highteenStickerList.includes(this.state.current_test)) {
-            const rederTestList = ["fifteenTheme", "searchResult", "zookeeper", "distributionTable", "correspondence",];
+        } else if (loveSimulationList.includes(this.state.current_test)) {
+            const rederTestList = ["fifteenTheme", "highteenSticker", "searchResult", "zookeeper", "distributionTable", "correspondence",];
             return (
                 <Fragment>
                     {rederTestList.map((test) => (
@@ -806,7 +806,7 @@ class Result extends Component {
         } else if (hmallList.includes(this.state.current_test)) {
             return null;
         } else {
-            const rederTestList = ["fifteenTheme", "highteenSticker", "searchResult", "zookeeper", "distributionTable",];
+            const rederTestList = ["fifteenTheme", "loveSimulation", "highteenSticker", "searchResult", "zookeeper", "distributionTable",];
             return(
                 <Fragment>
                     {rederTestList.map((test) => (
