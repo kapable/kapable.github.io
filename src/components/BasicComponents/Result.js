a8152c3a7d41fde95460abc3be369080d54e82c3
kapable <skjoldenstore@gmail.com>
2023-11-07 08:17:47 +0100

    add blogChar test

--- src/components/BasicComponents/Result.js
+++ src/components/BasicComponents/Result.js
@@ -430,7 +430,7 @@ class Result extends Component {
         let percentageMBTI2022ListKor = ["percentageMBTI2022",];
         let percentageMBTI2022ListOverseas = ["percentageMBTI2022Eng","percentageMBTI2022Cn"];
         let mindFlowEngList = ["mindFlowEng"];
-        let catoonCharList = ["catoonChar"];
+        let blogCharList = ["blogChar"];
         let hmallList = ['gardenflower', 'hmall', 'labelStickerSKT'];
         if(personalColor2022ListKor.includes(this.state.current_test)){
             const jelling_outlink = "https://bit.ly/3FlwKMJ";
@@ -552,8 +552,8 @@ class Result extends Component {
                     ))}
                 </Fragment>
             )
-        } else if (catoonCharList.includes(this.state.current_test)) {
-            const rederTestList = ["colorFiveTheme", "fifteenTheme", "switchOnOff", "colorVideo", "lovePepero", "receiptChar",];
+        } else if (blogCharList.includes(this.state.current_test)) {
+            const rederTestList = ["colorFiveTheme", "fifteenTheme", "catoonChar", "switchOnOff", "colorVideo", "lovePepero",];
             return (
                 <Fragment>
                     {rederTestList.map((test) => (
@@ -819,7 +819,7 @@ class Result extends Component {
         } else if (hmallList.includes(this.state.current_test)) {
             return null;
         } else {
-            const rederTestList = ["colorFiveTheme", "fifteenTheme", "catoonChar", "switchOnOff", "colorVideo", "lovePepero",];
+            const rederTestList = ["colorFiveTheme", "fifteenTheme", "blogChar", "catoonChar", "switchOnOff", "colorVideo",];
             return(
                 <Fragment>
                     {rederTestList.map((test) => (
