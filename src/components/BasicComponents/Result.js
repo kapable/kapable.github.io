8bf6a458e4e63b51f59b216472b797c1a92e0403
kapable <skjoldenstore@gmail.com>
2023-11-18 13:42:00 +0100

    add  theartWindowest

--- src/components/BasicComponents/Result.js
+++ src/components/BasicComponents/Result.js
@@ -430,7 +430,7 @@ class Result extends Component {
         let percentageMBTI2022ListKor = ["percentageMBTI2022",];
         let percentageMBTI2022ListOverseas = ["percentageMBTI2022Eng","percentageMBTI2022Cn"];
         let mindFlowEngList = ["mindFlowEng"];
-        let colorPenList = ["colorPen"];
+        let heartWindowList = ["heartWindow"];
         let hmallList = ['gardenflower', 'hmall', 'labelStickerSKT'];
         if(personalColor2022ListKor.includes(this.state.current_test)){
             const jelling_outlink = "https://bit.ly/3FlwKMJ";
@@ -552,8 +552,8 @@ class Result extends Component {
                     ))}
                 </Fragment>
             )
-        } else if (colorPenList.includes(this.state.current_test)) {
-            const rederTestList = ["colorFiveTheme", "colorSquare", "fifteenTheme", "colorChart", "naileage", "weekendSchedule", ];
+        } else if (heartWindowList.includes(this.state.current_test)) {
+            const rederTestList = ["colorFiveTheme", "fifteenTheme", "colorPen", "colorSquare", "colorChart",];
             return (
                 <Fragment>
                     {rederTestList.map((test) => (
@@ -819,7 +819,7 @@ class Result extends Component {
         } else if (hmallList.includes(this.state.current_test)) {
             return null;
         } else {
-            const rederTestList = ["colorFiveTheme", "fifteenTheme", "colorPen", "colorSquare", "colorChart", "naileage", "weekendSchedule", ];
+            const rederTestList = ["colorFiveTheme", "fifteenTheme", "heartWindow", "colorPen", "colorSquare",];
             return(
                 <Fragment>
                     {rederTestList.map((test) => (
