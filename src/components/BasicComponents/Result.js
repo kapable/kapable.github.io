76935a514a38accae963f88dbac127f4c7507426
kapable <skjoldenstore@gmail.com>
2024-01-24 13:46:36 +0900

    add nagForYou test

--- src/components/BasicComponents/Result.js
+++ src/components/BasicComponents/Result.js
@@ -431,7 +431,7 @@ class Result extends Component {
         let percentageMBTI2022ListKor = ["percentageMBTI2022",];
         let percentageMBTI2022ListOverseas = ["percentageMBTI2022Eng","percentageMBTI2022Cn"];
         let mindFlowEngList = ["mindFlowEng"];
-        let webCharList = ["webChar"]; 
+        let nagForYouList = ["nagForYou"]; 
         let hmallList = ['gardenflower', 'hmall', 'labelStickerSKT'];
         if(personalColor2022ListKor.includes(this.state.current_test)){
             const jelling_outlink = "https://bit.ly/3FlwKMJ";
@@ -553,8 +553,8 @@ class Result extends Component {
                     ))}
                 </Fragment>
             )
-        } else if (webCharList.includes(this.state.current_test)) {
-            const rederTestList = ["magazineTheme", "colorFiveTheme", "fifteenTheme", "letYouKnowYou", "guestCheck", "glitchChar",];
+        } else if (nagForYouList.includes(this.state.current_test)) {
+            const rederTestList = ["magazineTheme", "colorFiveTheme", "fifteenTheme", "webChar", "letYouKnowYou", "guestCheck",];
             return (
                 <Fragment>
                     {rederTestList.map((test) => (
@@ -820,7 +820,7 @@ class Result extends Component {
         } else if (hmallList.includes(this.state.current_test)) {
             return null;
         } else {
-            const rederTestList = ["magazineTheme", "colorFiveTheme", "fifteenTheme", "webChar", "letYouKnowYou", "guestCheck",];
+            const rederTestList = ["magazineTheme", "colorFiveTheme", "fifteenTheme", "nagForYou", "webChar", "letYouKnowYou",];
             return(
                 <Fragment>
                     {rederTestList.map((test) => (
