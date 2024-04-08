ff22ebba1934cee65b9fb053f047c767e4f50c0d
kapable <skjoldenstore@gmail.com>
2024-04-08 13:09:55 +0900

    change coupang banner design

--- src/components/BasicComponents/Result.js
+++ src/components/BasicComponents/Result.js
@@ -898,10 +898,26 @@ class Result extends Component {
         ];
         const testsArray = othersLink.map(({ test }) => test);
         return (
-            <div className='article-adCover-div-1'>
+            <div className='article-adCover-div-1'  style={{marginBottom:"2rem"}}>
                 <div className='article-adCover-div-2'>
-                    {/* Old Version */}
+                    {/* 2024 version */}
                     <div className='article-adCover-div-3'>
+                        <p><b><span style={{color:"#4185F4"}}>콘텐츠를 보기 전</span> 쿠팡 쇼핑을 해보세요</b></p>
+                        <div><p style={{ fontSize: '0.7rem' }}>쿠팡 방문은 케이테스트가 항상 질좋은 콘텐츠를 제공할 수 있는 힘이 됩니다.<br />항상 케이테스트 콘텐츠를 사랑해주셔서 감사합니다.</p></div>
+                        <p style={{ fontSize: "0.6rem", width: this.state.coupangCount === 0 ? "5.7rem" : "0.7rem", color: "white", backgroundColor: "rgba(0, 0, 0, 0.5)", position:"absolute", right:"0rem", bottom: "-2rem" }}>
+                            {this.state.startTimer ? (
+                                    this.state.coupangCount === 0 ? <span style={{color:'white', cursor:'pointer'}} onClick={this.state.coupangCount === 0 ? this.onCoupangCloseButtonClick : null}><b>광고 건너뛰기 &rarr;|</b></span> : this.state.coupangCount
+                            ) :null}
+                        </p>
+                        <a href={testsArray.includes(this.state.current_test) && otherAdProb ? othersLink.find((item) => item?.test === this.state.current_test)?.coupangLink : cookieRocketCoupangLink} target="_blank" rel='noreferrer noopener'>
+                            <button className='result-coupang-button' type="primary" shape='round' style={{ width: '15rem', height: '3.5rem'}} onClick={testsArray.includes(this.state.current_test) && otherAdProb ? this.onOtherCoupangButtonClick : () => this.onCoupangButtonClick(this.state.current_test)}>
+                                버튼 누르고 결과 보기
+                            </button>
+                        </a>
+                        <p style={{ fontSize: '10px', color: 'grey', marginTop: "0.5rem" }}>원치 않을 경우 뒤로 가기를 눌러주세요.</p>
+                    </div>
+                    {/* Old Version */}
+                    {/* <div className='article-adCover-div-3'>
                         <p><b><span style={{color:"#4185F4"}}>콘텐츠를 보기 전</span> 쿠팡 쇼핑을 해보세요</b></p>
                         <div><p style={{ fontSize: '0.7rem' }}>쿠팡 방문은 케이테스트가 항상 질좋은 콘텐츠를 제공 할 수 있는 힘이 됩니다.<br />항상 케이테스트 콘텐츠를 사랑해주셔서 감사합니다.</p></div>
                         <p style={{ fontSize: "0.7rem", width: "0.7rem", color: "white", backgroundColor: "#83b4de", position:"absolute", right:"4rem", top: "5.2rem" }}>
@@ -915,7 +931,7 @@ class Result extends Component {
                             </button>
                         </a>
                         <p style={{ fontSize: '10px', color: 'grey', marginTop: "0.5rem" }}>원치 않을 경우 뒤로 가기를 눌러주세요.</p>
-                    </div>
+                    </div> */}
                     {/* New Version */}
                     {/* <div className='article-adCover-div-3'>
                         <p style={{fontSize:'1rem'}}><b><span style={{color:'#0074E9'}}>인기 상품</span> 확인하고 결과 확인하세요!</b></p>
