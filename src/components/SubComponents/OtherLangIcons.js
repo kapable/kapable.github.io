bec580ccf3ee0f8635b13d70a3e0a40cb0fdac6c
kapable <skjoldenstore@gmail.com>
2024-04-16 16:07:05 +0900

    apply otherLangIcons for bitcoinChar test

--- src/components/SubComponents/OtherLangIcons.js
+++ src/components/SubComponents/OtherLangIcons.js
@@ -4930,6 +4930,34 @@ export default function OtherLangIcons(props) {
         }
     };
 
+    function bitcoinCharTestLinkRenderer(){
+        if(props.currentTest === "bitcoinChar") {
+            return(
+                <div className="go-to-each-language">
+                    <h3>▼OTHER LANGUAGES▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/bitcoinCharEng/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img loading="lazy" src='https://images.ktestone.com/default/languageIcon/usa-icon.png' className='language-icon' alt="[ Go to the English version ]" /></a>
+                </div>
+            )
+        } else if(props.currentTest === "bitcoinCharEng") {
+            return(
+                <div className="go-to-each-language">
+                    <h3>▼OTHER LANGUAGES▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/bitcoinChar/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img loading="lazy" src='https://images.ktestone.com/default/languageIcon/kor-icon.png' className='language-icon' alt="[ 한국어 버전으로 하러가기 ]" /></a>
+                </div>
+            )
+        }
+    };
+
     return (
         <Fragment>
             {factBTILinkRenderer()}
@@ -4997,6 +5025,7 @@ export default function OtherLangIcons(props) {
             {deepOceanOutlawTestLinkRenderer()}
             {meOtherTestLinkRenderer()}
             {mindFlowTestLinkRenderer()}
+            {bitcoinCharTestLinkRenderer()}
         </Fragment>
     );
 };
