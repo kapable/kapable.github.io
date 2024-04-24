c8996fa1a41eb33d7fcf7a9ce6ecf683e80e990a
kapable <skjoldenstore@gmail.com>
2024-04-24 20:26:08 +0900

    setting non normal routing in colorPicker result page

--- src/components/TestTypes/ColorPicker/ColorPickerResult.js
+++ src/components/TestTypes/ColorPicker/ColorPickerResult.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import { useLocation } from 'react-router-dom/cjs/react-router-dom.min'
+import React, { useState } from 'react'
+import { useHistory, useLocation } from 'react-router-dom/cjs/react-router-dom.min'
 
 const ColorPickerResult = () => {
   const location = useLocation();
+  const history = useHistory();
+  useState(() => {
+    if(!location.state) {
+      alert("잘못된 경로로 접근하셨습니다.");
+      history.push('/colorPicker');
+    }
+  }, [location]);
   return (
     // TODO: Design
     <div>{location.state}모드를 클리어 했다 너</div>
