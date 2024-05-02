fd6b8981bbcb616c3982525f4cb0b89f0dd2b5f5
kapable <skjoldenstore@gmail.com>
2024-05-02 11:29:49 +0900

    add ethereumCharEng test

--- src/components/SubComponents/OtherLangIcons.js
+++ src/components/SubComponents/OtherLangIcons.js
@@ -5114,6 +5114,34 @@ export default function OtherLangIcons(props) {
         }
     };
 
+    function ethereumCharTestLinkRenderer(){
+        if(props.currentTest === "ethereumChar") {
+            return(
+                <div className="go-to-each-language">
+                    <h3>▼OTHER LANGUAGES▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/ethereumCharEng/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img loading="lazy" src='https://images.ktestone.com/default/languageIcon/usa-icon.png' className='language-icon' alt="[ Go to the English version ]" /></a>
+                </div>
+            )
+        } else if(props.currentTest === "ethereumCharEng") {
+            return(
+                <div className="go-to-each-language">
+                    <h3>▼OTHER LANGUAGES▼</h3>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://ktestone.com/kapable.github.io/ethereumChar/"
+                        className="to-personalColorOut-test-banner-text"
+                    ><img loading="lazy" src='https://images.ktestone.com/default/languageIcon/kor-icon.png' className='language-icon' alt="[ 한국어 버전으로 하러가기 ]" /></a>
+                </div>
+            )
+        }
+    };
+
     return (
         <Fragment>
             {factBTILinkRenderer()}
@@ -5185,6 +5213,7 @@ export default function OtherLangIcons(props) {
             {whatRightTestLinkRenderer()}
             {pokerCharTestLinkRenderer()}
             {onigiriCharTestLinkRenderer()}
+            {ethereumCharTestLinkRenderer()}
         </Fragment>
     );
 };
