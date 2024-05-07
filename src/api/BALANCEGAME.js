4d7a8f23b3c073836044be27e23f4aa0b5b8a0a8
Kapable <seeyong987@gmail.com>
2024-05-07 14:32:20 +0900

    release loveBalance2 game

--- src/api/BALANCEGAME.js
+++ src/api/BALANCEGAME.js
@@ -26,7 +26,35 @@ export const balanceTests = [
                 options: [ '이성친구 둘이서 1박 2일 여행 가기 ', '전 애인과 단둘이 술 마시기',],
             },
         ],
-    }
+    },
+    {
+        info : {
+            mainTitle:"연애 밸런스 게임 2탄",
+            subTitle:"애정 박살 주의!",
+            mainUrl:"loveBalance2",
+            mainImage:"https://images.ktestone.com/introImages/loveBalance2-intro.jpg",
+            thumbImage:"https://images.ktestone.com/main-thumbnail/loveBalance2-thumb.png",
+            lang:"Kor",
+            category: "game",
+        },
+        questions: [
+            {
+                options: [ '1분마다 연락 안 되면 불안해하는 애인 ', '24시간 뒤 카톡 해도 관심 없는 애인',],
+            },
+            {
+                options: [ '매일 같이 번호 따이는 애인 ', '친구와 스킨십 하는 애인 (손잡기, 포옹, 어깨동무)',],
+            },
+            {
+                options: [ '힘든 거 다 숨기고 안 말하는 애인 ', '별거 아닌 걸로도 다 말하고 찡찡 거리는 애인',],
+            },
+            {
+                options: [ '매일 돈 없다고 말하는 애인 ', '돈은 많지만 잘 안 쓰는 짠돌이 애인',],
+            },
+            {
+                options: [ '친구의 전 애인과 사귀기 ', '친구의 현 애인과 사귀기',],
+            },
+        ],
+    },
 ];
 
 export const balanceTestsTitle = balanceTests.map((test) => test.info.mainUrl);
