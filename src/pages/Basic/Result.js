c987c47afed0abd7d7481cba1abcb58e417436f6
kapable <skjoldenstore@gmail.com>
2024-05-30 02:39:20 +0900

    filtering CN result about aliButton

--- src/pages/Basic/Result.js
+++ src/pages/Basic/Result.js
@@ -876,6 +876,7 @@ class Result extends Component {
     let img_src = _current_test_result.img_src;
     let test_current = _current_test_contents.info.mainTitle;
     let desc_test_current = _current_test_contents.info.subTitle;
+    let lang_test_current = _current_test_contents.info.lang;
 
     // return final result option
     if (
@@ -979,6 +980,61 @@ class Result extends Component {
           </Fragment>
         );
         // meta tag for native test contents
+      } else if (lang_test_current === 'CN') {
+        return (
+          <Fragment>
+            <Helmet>
+              {/* <!-- Primary Meta Tags --> */}
+              <title>{test_current}-케이테스트</title>
+              <meta name='title' content={test_current + '-케이테스트'} />
+              <meta
+                name='description'
+                content={this.state.current_result + ':' + desc_test_current}
+              />
+              <link rel='main-url' href={this.state.sharable_url} />
+
+              {/* <!-- Open Graph / Facebook --> */}
+              <meta property='og:type' content='website' />
+              <meta property='og:url' content={this.state.sharable_url} />
+              <meta
+                property='og:title'
+                content={test_current + '-케이테스트'}
+              />
+              <meta
+                property='og:description'
+                content={this.state.current_result + ':' + desc_test_current}
+              />
+              <meta property='og:image' content={img_src} />
+              <meta
+                property='og:image:alt'
+                content={this.state.current_result}
+              />
+
+              {/* <!-- Twitter --> */}
+              <meta property='twitter:card' content='summary_large_image' />
+              <meta property='twitter:url' content={this.state.sharable_url} />
+              <meta
+                property='twitter:title'
+                content={test_current + '-케이테스트'}
+              />
+              <meta
+                property='twitter:description'
+                content={this.state.current_result + ':' + desc_test_current}
+              />
+              <meta property='twitter:image' content={img_src} />
+              <meta
+                property='twitter:image:alt'
+                content={this.state.current_result}
+              />
+            </Helmet>
+            <img
+              loading='lazy'
+              src={img_src}
+              className='result-img'
+              alt={final_type}
+            />
+          </Fragment>
+        );
       } else if (this.state.current_test === 'union') {
         return (
           <Fragment>
