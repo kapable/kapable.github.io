f7b693b0e51aa37a84a2c83939da25efd6e6ceae
kapable <skjoldenstore@gmail.com>
2024-06-17 12:51:18 +0900

    add eggCharJP and colorBubbleLove test

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -9697,6 +9697,48 @@ export default function OtherLangIcons(props) {
     }
   }
 
+  function eggCharTestLinkRenderer() {
+    if (props.currentTest === 'eggChar') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/eggCharJP/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/japan-icon.png'
+              className='language-icon'
+              alt='[ 日本語バージョンをやりに行く。 ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'eggCharJP') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/eggChar/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+        </div>
+      );
+    }
+  }
+
   return (
     <Fragment>
       {factBTILinkRenderer()}
@@ -9774,6 +9816,7 @@ export default function OtherLangIcons(props) {
       {degreeWeatherCharTestLinkRenderer()}
       {blockYouTestLinkRenderer()}
       {talkAlotLoveTestLinkRenderer()}
+      {eggCharTestLinkRenderer()}
     </Fragment>
   );
 }
