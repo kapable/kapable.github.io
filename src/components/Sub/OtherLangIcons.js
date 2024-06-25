f12e0f9faf4b86da6abb2fb26bb42e7a9de0b8c5
kapable <skjoldenstore@gmail.com>
2024-06-25 13:11:22 +0900

    add plopPoolCharEng test

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -9865,6 +9865,48 @@ export default function OtherLangIcons(props) {
     }
   }
 
+  function plopPoolCharTestLinkRenderer() {
+    if (props.currentTest === 'plopPoolChar') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/plopPoolCharEng/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/usa-icon.png'
+              className='language-icon'
+              alt='[ Go to the English version ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'plopPoolCharEng') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/plopPoolChar/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+        </div>
+      );
+    }
+  }
+
   return (
     <Fragment>
       {factBTILinkRenderer()}
@@ -9946,6 +9988,7 @@ export default function OtherLangIcons(props) {
       {knittingTestLinkRenderer()}
       {monsterAttackTestLinkRenderer()}
       {highteenStickerTestLinkRenderer()}
+      {plopPoolCharTestLinkRenderer()}
     </Fragment>
   );
 }
