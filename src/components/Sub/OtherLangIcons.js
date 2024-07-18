77f5734626ab735a5bc23efe71e02951218b2bd1
kapable <skjoldenstore@gmail.com>
2024-07-18 11:29:23 +0900

    add christmasSleighEng test

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -10853,6 +10853,48 @@ export default function OtherLangIcons(props) {
     }
   }
 
+  function christmasSleighTestLinkRenderer() {
+    if (props.currentTest === 'christmasSleigh') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/christmasSleighEng/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/usa-icon.png'
+              className='language-icon'
+              alt='[ Go to the English version ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'christmasSleighEng') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/christmasSleigh/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+        </div>
+      );
+    }
+  }
+
   return (
     <Fragment>
       {factBTILinkRenderer()}
@@ -10952,6 +10994,7 @@ export default function OtherLangIcons(props) {
       {charFactPokPickTestLinkRenderer()}
       {cabinetCharTestLinkRenderer()}
       {zigzagCharTestLinkRenderer()}
+      {christmasSleighTestLinkRenderer()}
     </Fragment>
   );
 }
