caac4d1afa0f7f96b96b1174acd0c806af3c8796
kapable <skjoldenstore@gmail.com>
2024-07-22 21:28:31 +0900

    add balloonDartEng test

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -11063,6 +11063,48 @@ export default function OtherLangIcons(props) {
     }
   }
 
+  function balloonDartTestLinkRenderer() {
+    if (props.currentTest === 'balloonDart') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/balloonDartEng/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/usa-icon.png'
+              className='language-icon'
+              alt='[ Go to the English version ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'balloonDartEng') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/balloonDart/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+        </div>
+      );
+    }
+  }
+
   return (
     <Fragment>
       {factBTILinkRenderer()}
@@ -11167,6 +11209,7 @@ export default function OtherLangIcons(props) {
       {tanghuluTestLinkRenderer()}
       {watercolorCharTestLinkRenderer()}
       {systemErrorTestLinkRenderer()}
+      {balloonDartTestLinkRenderer()}
     </Fragment>
   );
 }
