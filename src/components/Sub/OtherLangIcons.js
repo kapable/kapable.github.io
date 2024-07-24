5bceccec81ed542b38d6d8a226b52dc665702eba
kapable <skjoldenstore@gmail.com>
2024-07-24 12:54:13 +0900

    add loveGeneEng test

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -11231,6 +11231,48 @@ export default function OtherLangIcons(props) {
     }
   }
 
+  function loveGeneTestLinkRenderer() {
+    if (props.currentTest === 'loveGene') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/loveGeneEng/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/usa-icon.png'
+              className='language-icon'
+              alt='[ Go to the English version ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'loveGeneEng') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/loveGene/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+        </div>
+      );
+    }
+  }
+
   return (
     <Fragment>
       {factBTILinkRenderer()}
@@ -11339,6 +11381,7 @@ export default function OtherLangIcons(props) {
       {blackCherryTestLinkRenderer()}
       {chatDataClassTestLinkRenderer()}
       {colorComboTestLinkRenderer()}
+      {loveGeneTestLinkRenderer()}
     </Fragment>
   );
 }
