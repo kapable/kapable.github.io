4c63ab22f08f316a838f1604a4432b8270acd42c
kapable <skjoldenstore@gmail.com>
2024-07-24 17:29:48 +0900

    add colorSquareEng test

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -11273,6 +11273,48 @@ export default function OtherLangIcons(props) {
     }
   }
 
+  function colorSquareTestLinkRenderer() {
+    if (props.currentTest === 'colorSquare') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/colorSquareEng/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/usa-icon.png'
+              className='language-icon'
+              alt='[ Go to the English version ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'colorSquareEng') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/colorSquare/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+        </div>
+      );
+    }
+  }
+
   return (
     <Fragment>
       {factBTILinkRenderer()}
@@ -11382,6 +11424,7 @@ export default function OtherLangIcons(props) {
       {chatDataClassTestLinkRenderer()}
       {colorComboTestLinkRenderer()}
       {loveGeneTestLinkRenderer()}
+      {colorSquareTestLinkRenderer()}
     </Fragment>
   );
 }
