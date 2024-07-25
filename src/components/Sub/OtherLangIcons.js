0dd7ec5c59c5c32267d0a3f942f9a4fe7a28e53c
kapable <skjoldenstore@gmail.com>
2024-07-25 12:35:18 +0900

    add chatSalesEng test

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -11315,6 +11315,48 @@ export default function OtherLangIcons(props) {
     }
   }
 
+  function chatSalesTestLinkRenderer() {
+    if (props.currentTest === 'chatSales') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/chatSalesEng/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/usa-icon.png'
+              className='language-icon'
+              alt='[ Go to the English version ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'chatSalesEng') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/chatSales/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+        </div>
+      );
+    }
+  }
+
   return (
     <Fragment>
       {factBTILinkRenderer()}
@@ -11425,6 +11467,7 @@ export default function OtherLangIcons(props) {
       {colorComboTestLinkRenderer()}
       {loveGeneTestLinkRenderer()}
       {colorSquareTestLinkRenderer()}
+      {chatSalesTestLinkRenderer()}
     </Fragment>
   );
 }
