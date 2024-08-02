6b2645d032ca4def7bf2b54f25a5b2c6a398a35b
kapable <skjoldenstore@gmail.com>
2024-08-02 21:05:33 +0900

    add loveLegoEng test

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -11441,6 +11441,48 @@ export default function OtherLangIcons(props) {
     }
   }
 
+  function loveLegoTestLinkRenderer() {
+    if (props.currentTest === 'loveLego') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/loveLegoEng/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/usa-icon.png'
+              className='language-icon'
+              alt='[ Go to the English version ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'loveLegoEng') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/loveLego/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+        </div>
+      );
+    }
+  }
+
   return (
     <Fragment>
       {factBTILinkRenderer()}
@@ -11554,6 +11596,7 @@ export default function OtherLangIcons(props) {
       {chatSalesTestLinkRenderer()}
       {checkMeSimpleTestLinkRenderer()}
       {colorVideoTestLinkRenderer()}
+      {loveLegoTestLinkRenderer()}
     </Fragment>
   );
 }
