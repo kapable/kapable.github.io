511fd82c1ceeca49a21e76ae29ab0f1c8b1f3b4f
kapable <skjoldenstore@gmail.com>
2024-10-10 14:17:04 +0900

    change myOwnFrame image ext and rearrange main page order

--- src/pages/Basic/MainPage.js
+++ src/pages/Basic/MainPage.js
@@ -418,7 +418,6 @@ const MainPage = ({ lang, category }) => {
         responsive='true'
         style={{ display: 'block' }}
       />
-      {shortAnswerQuizRenderer({ start: 3, end: 40 })}
       {/* Main Test Banners(Top) */}
       <div className='main-link-div'>
         {currentTestList.slice(0, render_range_points[0]).map((item, idx) => {
@@ -462,6 +461,7 @@ const MainPage = ({ lang, category }) => {
         </Link>
       ) : null}
 
+      {shortAnswerQuizRenderer({ start: 3, end: 40 })}
       {bingoGameRenderer()}
       {balanceTestRenderer()}
       {colorPickerRenderer()}
