e117c9826a05120ffaf6a6d25d33a9d64a4351ac
kapable <skjoldenstore@gmail.com>
2024-10-15 10:36:41 +0900

    add twinkleTeeniepingEng test

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -12329,6 +12329,48 @@ export default function OtherLangIcons(props) {
     }
   }
 
+  function twinkleTeeniepingTestLinkRenderer() {
+    if (props.currentTest === 'twinkleTeenieping') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/twinkleTeeniepingEng/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/usa-icon.png'
+              className='language-icon'
+              alt='[ Go to the English version ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'twinkleTeeniepingEng') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/twinkleTeenieping/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+        </div>
+      );
+    }
+  }
+
   return (
     <Fragment>
       {factBTILinkRenderer()}
@@ -12459,6 +12501,7 @@ export default function OtherLangIcons(props) {
       {loveEmergencyStopTestLinkRenderer()}
       {badHabitTestLinkRenderer()}
       {pingLoveTestLinkRenderer()}
+      {twinkleTeeniepingTestLinkRenderer()}
     </Fragment>
   );
 }
