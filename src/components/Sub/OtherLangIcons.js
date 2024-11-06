a6b13c14ee31907535a43616d16ca089f783430e
kapable <skjoldenstore@gmail.com>
2024-11-06 14:06:59 +0900

    add hideLoveEng test

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -12455,6 +12455,48 @@ export default function OtherLangIcons(props) {
     }
   }
 
+  function hideLoveTestLinkRenderer() {
+    if (props.currentTest === 'hideLove') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/hideLoveEng/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/usa-icon.png'
+              className='language-icon'
+              alt='[ Go to the English version ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'hideLoveEng') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/hideLove/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+        </div>
+      );
+    }
+  }
+
   return (
     <Fragment>
       {factBTILinkRenderer()}
@@ -12588,6 +12630,7 @@ export default function OtherLangIcons(props) {
       {twinkleTeeniepingTestLinkRenderer()}
       {myOwnFrameTestLinkRenderer()}
       {changeCharTestLinkRenderer()}
+      {hideLoveTestLinkRenderer()}
     </Fragment>
   );
 }
