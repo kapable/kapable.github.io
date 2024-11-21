14d9634e7375f12e06e680733445eb7cdf989202
kapable <skjoldenstore@gmail.com>
2024-11-21 10:38:02 +0900

    update LangIcons for myCookieClassJP

--- src/components/Sub/OtherLangIcons.js
+++ src/components/Sub/OtherLangIcons.js
@@ -12079,6 +12079,19 @@ export default function OtherLangIcons(props) {
               alt='[ Go to the English version ]'
             />
           </a>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/myCookieClassJP/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/japan-icon.png'
+              className='language-icon'
+              alt='[ 日本語バージョンをやりに行く。 ]'
+            />
+          </a>
         </div>
       );
     } else if (props.currentTest === 'myCookieClassEng') {
@@ -12096,6 +12109,51 @@ export default function OtherLangIcons(props) {
               src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
               className='language-icon'
               alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/myCookieClassJP/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/japan-icon.png'
+              className='language-icon'
+              alt='[ 日本語バージョンをやりに行く。 ]'
+            />
+          </a>
+        </div>
+      );
+    } else if (props.currentTest === 'myCookieClassJP') {
+      return (
+        <div className='go-to-each-language'>
+          <h3>▼OTHER LANGUAGES▼</h3>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/myCookieClass/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/kor-icon.png'
+              className='language-icon'
+              alt='[ 한국어 버전으로 하러가기 ]'
+            />
+          </a>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            href='https://ktestone.com/kapable.github.io/myCookieClassEng/'
+            className='to-personalColorOut-test-banner-text'
+          >
+            <img
+              loading='lazy'
+              src='https://images.ktestone.com/default/languageIcon/usa-icon.png'
+              className='language-icon'
+              alt='[ Go to the English version ]'
             />
           </a>
         </div>
